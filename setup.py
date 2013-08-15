796c1c153b74890ec250cd3a259a74961da673d9
Timothy Duly <timduly4@gmail.com>
2013-08-15 15:11:09 -0500

    v0.21: linux patch error with hwm07 is fixed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from numpy.distutils.core import setup
 #from setuptools import setup
 
 setup(name='pyglow',\
-        version='0.2',\
+        version='0.21',\
         url='github.com/timduly4/pyglow',\
         author='Timothy M. Duly',\
         author_email='duly2@illinois.edu',\
