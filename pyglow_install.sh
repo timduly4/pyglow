6e0083996ba8b3c2cedbcc59b44ababbca4bdf08
timduly4 <timothy.duly@spire.com>
2016-09-17 10:10:20 -0600

    pyglow_install.sh: added TODO fix

--- pyglow_install.sh
+++ pyglow_install.sh
@@ -7,7 +7,16 @@
 cd ./pyglow/models/; 
 make all; 
 cd ../../; 
+
+# (tmd, 9/17/16)
+# TODO: on a mac, we require:
+# --prefix=
+# to be appended for this installation command:
 python ./setup.py install --user;
+# How to check to see if we're on a mac?
+# Or, does this command work OK with Linux and we can
+# just add it by default?
+# Reference: https://goo.gl/7AG01z
 
 # Run update indices:
 cd ~/; 
