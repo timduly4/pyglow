193913bcf101ca345a9b40efb35d536c2984f1da
Timothy M. Duly <timduly4@gmail.com>
2019-01-11 18:25:47 -0700

    Addressing unicode errors with hwm07

--- src/pyglow/models/f2py/hwm07/remove_decode_errors.py
+++ src/pyglow/models/f2py/hwm07/remove_decode_errors.py
@@ -1,15 +1,7 @@
-import os
-
 import glob
 
 forfiles = glob.glob('*.f90')
 
 for f in forfiles:
-	content = open(f,'r',errors='replace').read()
-	open(f,'w').write(content)
-
-
-# this perl command deletes all the comments in iriflip...
-# my compiler was having trouble with them, for some reason
-
-
+    content = open(f, 'r').read()
+    open(f, 'w').write(unicode(content, errors='ignore'))
