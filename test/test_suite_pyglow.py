1adb76566efdc52a63ca365175bb23dce09d1179
Timothy M. Duly <timduly4@gmail.com>
2019-01-11 18:26:12 -0700

    Simplified test suite

--- test/test_suite_pyglow.py
+++ test/test_suite_pyglow.py
@@ -1,20 +1,16 @@
 import unittest
 
-from . import test_iri
-from . import test_hwm
-from . import test_msis
-from . import test_point
-from . import test_indices
+from .test_iri import TestIri
+from .test_hwm import TestHwm
+from .test_msis import TestMSIS
+from .test_point import TestPoint
+from .test_indices import TestIndice
 
-loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # Tests from each module:
-suite.addTests(loader.loadTestsFromModule(test_iri))
-suite.addTests(loader.loadTestsFromModule(test_hwm))
-suite.addTests(loader.loadTestsFromModule(test_msis))
-suite.addTests(loader.loadTestsFromModule(test_point))
-suite.addTests(loader.loadTestsFromModule(test_indices))
-
-runner = unittest.TextTestRunner(verbosity=0)
-result = runner.run(suite)
+suite.addTest(unittest.makeSuite(TestIri))
+suite.addTest(unittest.makeSuite(TestHwm))
+suite.addTest(unittest.makeSuite(TestMSIS))
+suite.addTest(unittest.makeSuite(TestPoint))
+suite.addTest(unittest.makeSuite(TestIndice))
